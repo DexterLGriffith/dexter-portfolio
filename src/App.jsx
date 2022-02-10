5a9ad21c457ef95bca277472921a0e3f0a1090b3
Dexter Griffith <dextergriffith@sbcglobal.net>
2022-02-09 21:14:38 -0600

    adding these files to github since ive been working on my local side

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+import About from "./components/About";
+
+
+const App = () => {
+
+  
+  return (
+    <div> 
+      <Home/>
+      <About/>
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,32 @@
+import "./home.css";
+import dexter from "../../photos/dexter2.png";
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home-left">
+                <div className="home-left-wrapper">
+                    <h2 className="home-introduction"> Hello, My name is </h2>
+                    <h1 className="home-name">Dexter Griffith </h1>
+                    <div className="home-title">
+                        <div className="home-title-wrapper">
+                            <div className="home-title-item">Web Developer</div>
+                            <div className="home-title-item">Front-End Enthusiast</div>
+                            <div className="home-title-item">Artist</div>
+                            <div className="home-title-item">Content Creator</div>
+                        </div>
+                    </div>
+                    <div className="home-bio">
+                    I am a Jr. Full Stack Web Developer with a vast array of knowledge in both frontend and backend languages.
+                    </div>
+                </div>
+            </div>
+            <div className="home-right">
+                <div className="home-right-bg"></div>
+                <img src={dexter} alt="" className="home-img" />
+            </div>
+        </div>
+    )
+}
+
+
+export default Home; 
