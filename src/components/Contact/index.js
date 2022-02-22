83ac6b40f30e553c90c62661e41aa546b4d3f6bb
Dexter Griffith <dextergriffith@sbcglobal.net>
2022-02-22 11:48:45 -0600

    working on contact me section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,67 @@
 import React from 'react';
+import "./contact.css";
+import { InputGroup, FormControl, Row, Col, Container } from 'react-bootstrap'
 
 function Contact() {
-    return(
-        <div>Contact</div>
+    return (
+        <div className='contactContainer'>
+            <h1 className='contactGreet'>Want to get in touch? <br /> Fill Out the form below!</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <InputGroup className="contactInfo">
+                            <FormControl
+                                placeholder="First Name*"
+                                aria-label="First Name"
+                                aria-describedby="basic-addon2"
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col>
+                        <InputGroup className="contactInfo">
+                            <FormControl
+                                placeholder="Last Name*"
+                                aria-label="Last Name"
+                                aria-describedby="basic-addon2"
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <InputGroup className="contactInfo">
+                            <FormControl
+                                placeholder="Email*"
+                                aria-label="Email"
+                                aria-describedby="basic-addon2"
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col>
+                        <InputGroup className="contactInfo">
+                            <FormControl
+                                placeholder="Phone Number"
+                                aria-label="Phone Number"
+                                aria-describedby="basic-addon2"
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <InputGroup>
+                            <FormControl as="textarea"
+                                placeholder="leave a brief comment"
+                                aria-label="With textarea"
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+
+            </Container>
+
+
+        </div>
     )
 }
 
