1fcffc447d1980467dbca9c4f3e23f28efc0f912
Dexter Griffith <dextergriffith@sbcglobal.net>
2022-02-22 12:29:15 -0600

    finishing up contact me page... doesnt submit... but the function is there

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./contact.css";
-import { InputGroup, FormControl, Row, Col, Container } from 'react-bootstrap'
+import { InputGroup, FormControl, Row, Col, Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 function Contact() {
     return (
+        <div className='contactBg'>
         <div className='contactContainer'>
             <h1 className='contactGreet'>Want to get in touch? <br /> Fill Out the form below!</h1>
             <Container>
@@ -57,10 +59,18 @@ function Contact() {
                         </InputGroup>
                     </Col>
                 </Row>
+                <Row>
+                    <Col>
 
+                        <Button variant="outline-dark" size="lg">
+                            Lets get in contact!
+                        </Button>
+                    </Col>
+                </Row>
             </Container>
 
 
+        </div >
         </div>
     )
 }
