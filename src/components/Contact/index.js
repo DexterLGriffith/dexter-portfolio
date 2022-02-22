936624ade715ed88a60c60a27eff35923d385fcc
Dexter Griffith <dextergriffith@sbcglobal.net>
2022-02-22 13:41:57 -0600

    added a send email for after you click the button to send me user information, however the link is broken.. somewhere..

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,20 +2,35 @@ import React from 'react';
 import "./contact.css";
 import { InputGroup, FormControl, Row, Col, Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
 
+function sendEmail(e) { 
+    e.preventDefault();
+
+    emailjs.sendForm('service_70fn5d9',
+    'template_mane61l',
+    e.target,
+    'user_5I4Mqm5Wjfpq6U0vgGvwG'
+    ).then(res=>{
+        console.log(res);
+    }).catch(err=> console.log(err));
+}
 function Contact() {
+
+
     return (
         <div className='contactBg'>
         <div className='contactContainer'>
             <h1 className='contactGreet'>Want to get in touch? <br /> Fill Out the form below!</h1>
-            <Container>
+            <Container onSubmit={sendEmail}>
                 <Row>
                     <Col>
-                        <InputGroup className="contactInfo">
+                        <InputGroup className="contactInfo" >
                             <FormControl
                                 placeholder="First Name*"
                                 aria-label="First Name"
                                 aria-describedby="basic-addon2"
+                                type="text" name ="first-name"
                             />
                         </InputGroup>
                     </Col>
@@ -25,6 +40,7 @@ function Contact() {
                                 placeholder="Last Name*"
                                 aria-label="Last Name"
                                 aria-describedby="basic-addon2"
+                                type="text" name="last-name"
                             />
                         </InputGroup>
                     </Col>
@@ -36,6 +52,7 @@ function Contact() {
                                 placeholder="Email*"
                                 aria-label="Email"
                                 aria-describedby="basic-addon2"
+                                type="email" name="user-email"
                             />
                         </InputGroup>
                     </Col>
@@ -45,6 +62,7 @@ function Contact() {
                                 placeholder="Phone Number"
                                 aria-label="Phone Number"
                                 aria-describedby="basic-addon2"
+                                type="text" name="user-number"
                             />
                         </InputGroup>
                     </Col>
@@ -52,9 +70,10 @@ function Contact() {
                 <Row>
                     <Col>
                         <InputGroup>
-                            <FormControl as="textarea"
+                            <FormControl 
                                 placeholder="leave a brief comment"
                                 aria-label="With textarea"
+                                type="text" name ="message"
                             />
                         </InputGroup>
                     </Col>
@@ -62,7 +81,7 @@ function Contact() {
                 <Row>
                     <Col>
 
-                        <Button variant="outline-dark" size="lg">
+                        <Button variant="outline-dark" size="lg" input type="submit" value="send" >
                             Lets get in contact!
                         </Button>
                     </Col>
