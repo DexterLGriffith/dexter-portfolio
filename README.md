74a941daffefdb6a29ef32afd57ed909be744896
Dexter Griffith <dextergriffith@sbcglobal.net>
2022-02-23 20:36:23 -0600

    starting resume section:

--- README.md
+++ README.md
@@ -29,5 +29,5 @@ Dexter Griffith
 3. https://react-bootstrap.netlify.app/components/navbar/#scrolling
 4. https://fonts.google.com/specimen/Lobster+Two?preview.text=Hello,%20my%20name%20is%20Dexter%20Griffith&preview.text_type=custom
 5. https://react-bootstrap.netlify.app/components/carousel/#api
-6. 
-7. 
+6. https://www.geeksforgeeks.org/javascript-pass-string-parameter-in-onclick-function/
+7. https://www.w3.org/TR/html401/interact/forms.html
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import "./resume.css";
+import github from '../../photos/logos/GitHub-Mark-64px.png';
+import linkedin from '../../photos/logos/LI-In-Bug.png';
+import gmail from '../../photos/logos/gmail-logo.png';
+import resume from '../../photos/logos/resume-clipart.png';
+import phone from '../../photos/logos/phone.png';
+import { Row, Col, Container } from 'react-bootstrap';
 
 function Resume() {
-    return(
-        <div>
-            <h1>Resume</h1>
-         </div>
+    return (
+        <div className='resume'>
+            <div className="workHistory">
+                <div className='workHistorySection'>
+                    <h1> Work History </h1>
+                </div>
+                <div className='proficiencySection'>
+                    <h1> Proficiency </h1>
+                </div>
+            </div>
+
+            <div className="professionalSkills">
+                <div className='professionalSkillsSection'>
+                    <h1> Professional Skills </h1>
+                </div>
+                <div className='socialMediaSection'>
+                    <h1 className='socialMedia'> Social Media </h1>
+                    <Container>
+                        <contactRow md={4}>
+                            <Col>
+                                <a link href="https://github.com/DexterLGriffith"><img src={github} alt="githubicon" /></a>
+                            </Col>
+                        </contactRow>
+                        <contactRow md={4}>
+                            <Col>
+                            <a link href="https://www.linkedin.com/in/dexter-griffith/"><img src={linkedin} alt="linkdinicon" style={{height:'60px',}}/></a>
+                            </Col>
+                        </contactRow>
+                        <contactRow md={4}>
+                            <Col>
+                            <a link href="mailto:dexterlgriffith@gmail.com"><img src={gmail} alt="gmailicon" style={{height:'60px',}}/></a>
+                            </Col>
+                        </contactRow>
+                        <contactRow md={4}>
+                            <Col>
+                            <a link href="https://zety.com/profile/dexter-griffith"><img src={resume} alt="resumeicon" style={{height:'50px',}}/></a>
+                            </Col>
+                        </contactRow>
+                        <contactRow md={4}>
+                            <Col>
+                            <a link href="tel:2143260125"><img src={phone} alt="phoneicon" style={{height:'60px',}}/></a>
+                            </Col>
+                        </contactRow>
+                    </Container>
+
+                </div>
+            </div>
+        </div>
 
     )
 }
