8cf77ba5f425c3e703a2fd8c947c849259a7f6c8
Dexter Griffith <dextergriffith@sbcglobal.net>
2022-02-24 01:50:30 -0600

    added pictures to projects tabs, and links to projects.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,6 +4,9 @@ import sports from "../../photos/projectImages/sportsScriptedImage.png";
 import daytrip from "../../photos/projectImages/daytripperImage.png";
 import lift from "../../photos/projectImages/liftRiteImage.png";
 import note from "../../photos/projectImages/notetakerImage.png";
+import progress from "../../photos/projectImages/progressive.png";
+import workout from "../../photos/projectImages/workoutapp.png";
+
 import './project.css';
 
 
@@ -19,6 +22,7 @@ function Projects() {
           />
           <Carousel.Caption>
             <h3>Lift Rite</h3>
+            <a href="https://liftrite.herokuapp.com/">Link to Lift Rite Application</a>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -30,6 +34,7 @@ function Projects() {
 
           <Carousel.Caption>
             <h3>Day Tripper</h3>
+            <a href="https://candracodes.github.io/daytripper/">Link to Day Tripper Application</a>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -41,39 +46,43 @@ function Projects() {
 
           <Carousel.Caption>
             <h3>Sports Scripted</h3>
+            <a href="https://sports-scripted.herokuapp.com/">Link to Sports Scripted Application</a>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="projectArt"
-            src={sports}
+            src={workout}
             alt="Fourth slide"
           />
 
           <Carousel.Caption>
             <h3>Workout Tracker</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="projectArt"
-            src={sports}
-            alt="Fifth slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Note Taker</h3>
+            <a href="https://workout-tracker111.herokuapp.com/">Link to Workout Tracker Application</a>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="projectArt"
             src={note}
+            alt="Fifth slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Note Taker</h3>
+            <a href="https://dexterlgriffith.github.io/Note-Taker/">Link to Note Taker Application</a>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="projectArt"
+            src={progress}
             alt="Sixth slide"
           />
 
           <Carousel.Caption>
             <h3>Progressive Budget</h3>
+            <a href="https://aqueous-atoll-42029.herokuapp.com/">Link to Progressive Budget Application</a>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
